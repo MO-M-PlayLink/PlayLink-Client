785caed41d0f9a3356ee6ddaf3c13ca3cd6e86f5
Juha Yoon <ijooha16@gmail.com>
2025-10-06 13:01:21 +0900

    .

--- src/app/authorized/match/create-match/create/page.tsx
+++ src/app/authorized/match/create-match/create/page.tsx
@@ -2,7 +2,7 @@ import CreateMatchForm from '@/components/forms/create-match-form';
 
 const CreateMatchPage = () => {
   return (
-    <div className='flex flex-col gap-s-16 pt-s-24'>
+    <div className='flex flex-col gap-s-16'>
       <CreateMatchForm />
     </div>
   );
